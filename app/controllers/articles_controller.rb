777bce78822b7a2d1b9a509fe9090e9bc4c6d9b7
Stefanie Cruz <stefanie.cruz@openapp.ie>
2022-06-22 14:55:08 +0100

    Add comment model & finish crud operations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -39,12 +39,12 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
 
-    redirect root_path, status: :see_other
+    redirect_to root_path, status: :see_other
   end
 
   private
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :status)
   end
 
 end
